d4fe3e37fa9c7b24cea6cc405ca8d4db91eeece1
nikhil reddy <jnikhilreddy219@gmail.com>
2025-04-22 20:00:57 +0530

    setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-red-500">Hello Netflix GPT!</div>;
+  return (
+    <div className="font-bold text-3xl text-green-800">Hello Netflix GPT!</div>
+  );
 }
 
 export default App;
